d4b50ee5bdd41d8d81fc22db0e1220673863b3c1
Markus Moeller <markus.moeller@desy.de>
2013-09-18 12:07:29 +0200

    o.c.utility.screenshot.desy: Small changes

--- applications/plugins/org.csstudio.utility.screenshot/src/org/csstudio/utility/screenshot/ScreenshotWorker.java
+++ applications/plugins/org.csstudio.utility.screenshot/src/org/csstudio/utility/screenshot/ScreenshotWorker.java
@@ -1,44 +1,54 @@
 
-/* 
- * Copyright (c) 2007 Stiftung Deutsches Elektronen-Synchrotron, 
+/*
+ * Copyright (c) 2007 Stiftung Deutsches Elektronen-Synchrotron,
  * Member of the Helmholtz Association, (DESY), HAMBURG, GERMANY.
  *
- * THIS SOFTWARE IS PROVIDED UNDER THIS LICENSE ON AN "../AS IS" BASIS. 
- * WITHOUT WARRANTY OF ANY KIND, EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED 
- * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR PARTICULAR PURPOSE AND 
- * NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
- * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR 
- * THE USE OR OTHER DEALINGS IN THE SOFTWARE. SHOULD THE SOFTWARE PROVE DEFECTIVE 
- * IN ANY RESPECT, THE USER ASSUMES THE COST OF ANY NECESSARY SERVICING, REPAIR OR 
- * CORRECTION. THIS DISCLAIMER OF WARRANTY CONSTITUTES AN ESSENTIAL PART OF THIS LICENSE. 
+ * THIS SOFTWARE IS PROVIDED UNDER THIS LICENSE ON AN "../AS IS" BASIS.
+ * WITHOUT WARRANTY OF ANY KIND, EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR PARTICULAR PURPOSE AND
+ * NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE. SHOULD THE SOFTWARE PROVE DEFECTIVE
+ * IN ANY RESPECT, THE USER ASSUMES THE COST OF ANY NECESSARY SERVICING, REPAIR OR
+ * CORRECTION. THIS DISCLAIMER OF WARRANTY CONSTITUTES AN ESSENTIAL PART OF THIS LICENSE.
  * NO USE OF ANY SOFTWARE IS AUTHORIZED HEREUNDER EXCEPT UNDER THIS DISCLAIMER.
- * DESY HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, 
+ * DESY HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS,
  * OR MODIFICATIONS.
- * THE FULL LICENSE SPECIFYING FOR THE SOFTWARE THE REDISTRIBUTION, MODIFICATION, 
- * USAGE AND OTHER RIGHTS AND OBLIGATIONS IS INCLUDED WITH THE DISTRIBUTION OF THIS 
- * PROJECT IN THE FILE LICENSE.HTML. IF THE LICENSE IS NOT INCLUDED YOU MAY FIND A COPY 
+ * THE FULL LICENSE SPECIFYING FOR THE SOFTWARE THE REDISTRIBUTION, MODIFICATION,
+ * USAGE AND OTHER RIGHTS AND OBLIGATIONS IS INCLUDED WITH THE DISTRIBUTION OF THIS
+ * PROJECT IN THE FILE LICENSE.HTML. IF THE LICENSE IS NOT INCLUDED YOU MAY FIND A COPY
  * AT HTTP://WWW.DESY.DE/LEGAL/LICENSE.HTM
  */
 
 package org.csstudio.utility.screenshot;
 
-import java.util.*;
+import java.util.Hashtable;
 import org.csstudio.utility.screenshot.internal.localization.ScreenshotMessages;
-import org.csstudio.utility.screenshot.util.*;
+import org.csstudio.utility.screenshot.util.PaintSurface;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.*;
-import org.eclipse.swt.layout.*;
-import org.eclipse.swt.printing.*;
-import org.eclipse.swt.widgets.*;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.printing.PrintDialog;
+import org.eclipse.swt.printing.Printer;
+import org.eclipse.swt.printing.PrinterData;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 
 /**
  * @author Markus Moeller based on the SWT paint example
  *
  */
-public class ScreenshotWorker
-{
+public class ScreenshotWorker {
+
     private Hashtable<String, String> messageContent = null;
     private Display display;
     private Composite mainComposite;
@@ -47,22 +57,21 @@ public class ScreenshotWorker
     private ImageBundle imageBundle = null;
     private PaintSurface paintSurface; // paint surface for drawing
     private int restart = 0;
-    
+
     /** */
     private int startDelay  = 0;
 
     /** */
     private int timer = 0;
-    
+
     /** */
     private boolean restartWithBeep = false;
     private boolean beep = false;
 
     private static final int numPaletteRows = 3;
     private static final int numPaletteCols = 50;
-    
-    public ScreenshotWorker(Composite p)
-    {
+
+    public ScreenshotWorker(Composite p) {
         this.mainComposite = p;
         this.display = PlatformUI.getWorkbench().getDisplay();
         initResources();
@@ -82,14 +91,11 @@ public class ScreenshotWorker
     /**
      * Creates the GUI.
      */
-    public void createGUI(Composite parent)
-    {
-        GridLayout gridLayout;
-        GridData gridData;
+    public void createGUI(Composite parent) {
 
-        /*** Create principal GUI layout elements ***/      
+        /*** Create principal GUI layout elements ***/
         Composite displayArea = new Composite(parent, SWT.NONE);
-        gridLayout = new GridLayout();
+        GridLayout gridLayout = new GridLayout();
         gridLayout.numColumns = 1;
         displayArea.setLayout(gridLayout);
 
@@ -99,19 +105,19 @@ public class ScreenshotWorker
         // instantiate all of the non-GUI application elements to satisfy referential
         // dependencies.  It is possible to reorder the initialization to some extent, but
         // this can be very tedious.
-        
+
         // paint canvas
         Canvas paintCanvas = new Canvas(displayArea, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL | SWT.NO_REDRAW_RESIZE | SWT.NO_BACKGROUND);
-        gridData = new GridData(GridData.FILL_HORIZONTAL | GridData.FILL_VERTICAL);
+        GridData gridData = new GridData(GridData.FILL_HORIZONTAL | GridData.FILL_VERTICAL);
         paintCanvas.setLayoutData(gridData);
         // paintCanvas.setBackground(paintColorWhite);
-        
+
         // status text
         // final Text statusText = new Text(displayArea, SWT.BORDER | SWT.SINGLE | SWT.READ_ONLY);
         // gridData = new GridData(GridData.FILL_HORIZONTAL | GridData.VERTICAL_ALIGN_FILL);
         // statusText.setLayoutData(gridData);
 
-        /*** Create the remaining application elements inside the principal GUI layout elements ***/    
+        /*** Create the remaining application elements inside the principal GUI layout elements ***/
         // paintSurface
         // paintSurface = new SWTPaintSurface(paintCanvas, statusText, paintColorWhite, imageBundle);
         paintSurface = new PaintSurface(paintCanvas, imageBundle);
@@ -120,23 +126,20 @@ public class ScreenshotWorker
     /**
      * Disposes of all resources associated with a particular
      * instance of the ScreenshotWorker.
-     */ 
-    public void dispose()
-    {
-        if (paintSurface != null)
-        {
-            paintSurface.dispose();       
+     */
+    public void dispose() {
+        if (paintSurface != null) {
+            paintSurface.dispose();
         }
-                
-        if(imageBundle != null)
-        {
+
+        if(imageBundle != null) {
             imageBundle.dispose();
             imageBundle = null;
         }
-        
+
         paintColors = null;
         paintSurface = null;
-        
+
         freeResources();
     }
 
@@ -146,14 +149,13 @@ public class ScreenshotWorker
     public void freeResources() {
     	//TODO:
     }
-    
+
     /**
      * Returns the Display.
-     * 
+     *
      * @return the display we're using
      */
-    public Display getDisplay()
-    {
+    public Display getDisplay() {
         return mainComposite.getDisplay();
     }
 
@@ -161,18 +163,18 @@ public class ScreenshotWorker
      * Initialize colors, fonts, and tool settings.
      */
     private void init() {
-        
+
     	Display disp = mainComposite.getDisplay();
-        
+
         paintColorWhite = new Color(disp, 255, 255, 255);
         paintColorBlack = new Color(disp, 0, 0, 0);
-        
+
         paintColors = new Color[numPaletteCols * numPaletteRows];
         paintColors[0] = paintColorBlack;
         paintColors[1] = paintColorWhite;
         for (int i = 2; i < paintColors.length; i++) {
             paintColors[i] = new Color(disp,
-                ((i * 7) % 255), ((i * 23) % 255), ((i * 51) % 255));
+                i * 7 % 255, i * 23 % 255, i * 51 % 255);
         }
     }
 
@@ -186,11 +188,11 @@ public class ScreenshotWorker
     /**
      * Loads the image resources.
      */
-    
+
     public void initResources() {
     	//TODO:
     }
-    
+
     /**
      * Grabs input focus.
      */
@@ -198,13 +200,13 @@ public class ScreenshotWorker
     {
         mainComposite.setFocus();
     }
-        
+
     public void capture()
     {
         if(startDelay == 0)
         {
             processCapture();
-            
+
             return;
         }
 
@@ -213,7 +215,7 @@ public class ScreenshotWorker
             timer = 0;
             display.beep();
             display.timerExec(1000, new Runnable()
-            {   
+            {
                 public void run()
                 {
                     beep();
@@ -224,7 +226,7 @@ public class ScreenshotWorker
         else
         {
             display.timerExec(startDelay, new Runnable()
-            { 
+            {
                     public void run()
                     {
                         processCapture();
@@ -232,15 +234,15 @@ public class ScreenshotWorker
             });
         }
     }
-    
+
     public void beep()
     {
         timer += 1000;
-        
+
         if(timer < startDelay)
         {
             display.beep();
-            
+
             display.timerExec(1000, new Runnable()
             {
                 public void run()
@@ -259,10 +261,10 @@ public class ScreenshotWorker
     {
         ImageCreator ic = new ImageCreator(this.display);
         imageBundle = new ImageBundle();
-        
-        ic.captureImages(imageBundle);        
+
+        ic.captureImages(imageBundle);
     }
-    
+
     public boolean performPrint(Shell shell, Image image)
     {
         GC gc = null;
@@ -270,7 +272,7 @@ public class ScreenshotWorker
 
         PrintDialog dialog = new PrintDialog(shell);
         PrinterData pd = dialog.open();
-        
+
         if(pd != null)
         {
             Printer printer = new Printer(pd);
@@ -282,16 +284,16 @@ public class ScreenshotWorker
             int             yScale  = dpi.y / 96;
             int             width   = bounds.width * xScale;
             int             height  = bounds.height * yScale;
-            int             pWidth  = area.width - (5 * dpi.x) / 4;
-            int             pHeight = area.height - (5 * dpi.x) / 4;
+            int             pWidth  = area.width - 5 * dpi.x / 4;
+            int             pHeight = area.height - 5 * dpi.x / 4;
             float           factor  = Math.min(1.0F, Math.min((float)pWidth / (float)width, (float)pHeight / (float)height));
-            int             aWidth  = (int)(factor * (float)width);
-            int             aHeight = (int)(factor * (float)height);
+            int             aWidth  = (int)(factor * width);
+            int             aHeight = (int)(factor * height);
             int             xoff    = (area.width - aWidth) / 2;
             int             yoff    = (area.height - aHeight) / 2;
-            
+
             String jobName = ScreenshotMessages.getString("ScreenshotPlugin.Screenshot");
-            
+
             if(printer.startJob(jobName))
             {
                 System.out.println(" Job gestartet\n");
@@ -301,61 +303,49 @@ public class ScreenshotWorker
                     System.out.println(" Seite gestartet\n");
 
                     System.out.println(image.toString() + ", " + bounds.x + ", " + bounds.y + ", " + bounds.width + ", " + bounds.height + ", " + xoff + ", " + yoff + ", " + aWidth + ", " + aHeight);
-                    
+
                     gc = new GC(printer);
-   
+
                     System.out.println(gc.toString());
 
                     gc.drawImage(image, bounds.x, bounds.y, bounds.width, bounds.height, xoff, yoff, aWidth, aHeight);
-                    
-                    printer.endPage();                    
+
+                    printer.endPage();
                 }
-                
+
                 printer.endJob();
-            }
-            else
-            {
+            } else {
                 success = false;
             }
-            
-            if(gc != null)
-            {
+
+            if(gc != null) {
                 gc.dispose();
             }
-            
-            if(printer != null)
-            {
-                printer.dispose();
-            }
+
+            printer.dispose();
         }
-        
+
         return success;
     }
 
-    public void setBeep(boolean beeping)
-    {
+    public void setBeep(boolean beeping) {
         beep = beeping;
     }
-    
-    public boolean getBeep()
-    {
+
+    public boolean getBeep() {
         return beep;
     }
 
-    public Image getSimpleImage()
-    {
+    public Image getSimpleImage() {
         return paintSurface.getImage();
     }
-    
-    public Image getDisplayedImage()
-    {
+
+    public Image getDisplayedImage() {
         return imageBundle.getDisplayedImage();
     }
-    
-    public void setDisplayedImage(Image i)
-    {
+
+    public void setDisplayedImage(Image i) {
         imageBundle.setDisplayedImage(i);
-        
         paintSurface.syncScrollBars();
     }
 
@@ -364,50 +354,42 @@ public class ScreenshotWorker
         paintSurface.setCapturedImage(i);
     }*/
 
-    public PaintSurface getPaintSurface()
-    {
+    public PaintSurface getPaintSurface() {
         return paintSurface;
     }
-    
-    public ImageBundle getAllImages()
-    {
+
+    public ImageBundle getAllImages() {
         return imageBundle;
     }
 
-    public void setRestartTime(int s)
-    {
+    public void setRestartTime(int s) {
         restart = s;
     }
-    
-    public int getRestartTime()
-    {
+
+    public int getRestartTime() {
         return restart;
     }
-    
-    public void setMessageContent(Hashtable<String, String> m)
-    {
+
+    public void setMessageContent(Hashtable<String, String> m) {
         messageContent = m;
     }
-    
-    public Hashtable<String, String> getMessageContent()
-    {
+
+    public Hashtable<String, String> getMessageContent() {
         return messageContent;
     }
-    
-    public String createXmlFromMessageContent()
-    {
+
+    public String createXmlFromMessageContent() {
+        // TODO: ????
         String  result = null;
-        
+
         return result;
     }
-    
-    public String getNameAndVersion()
-    {
+
+    public String getNameAndVersion() {
         return ScreenshotPlugin.getDefault().getNameAndVersion();
     }
-    
-    public Shell getShell()
-    {
+
+    public Shell getShell() {
         return ScreenshotPlugin.getDefault().getShell();
     }
 }
